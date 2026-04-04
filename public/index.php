f6c74838dee40f30cf11e1565e93ef4c81b212d2
Björn Büttner <Idrinth@users.noreply.github.com>
2026-04-04 17:46:47 +0200

    first post

--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
 <?php
+
+define("ROOT_DIR", dirname(__DIR__));
+function findAndExit(string $uri, string $language): void
+{
+    $path = ROOT_DIR . str_replace('//', '/', '/output/' . $uri . '/');
+    if (is_file($path . $language . '.html')) {
+        exit(file_get_contents($path . $language . '.html'));
+    }
+    if (is_file($path . 'en.html')) {
+        exit(file_get_contents($path . '/en.html'));
+    }
+}
+$uri = rtrim($_SERVER['REQUEST_URI'] ?? '', '/');
+$language = 'en';
+if ($uri === '') {
+    findAndExit($uri, $language);
+}
+if (!str_contains($uri, '.')) {
+    findAndExit($uri, $language);
+}
+findAndExit('404', $language);

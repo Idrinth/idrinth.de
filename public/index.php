6c5dd5a07bbc2d9fdd9c35e9a131fc5236bfc2b8
Björn Büttner <Idrinth@users.noreply.github.com>
2026-04-04 19:09:26 +0200

    routing improvements

--- public/index.php
+++ public/index.php
@@ -5,29 +5,33 @@ function findAndExit(string $uri, string $language): void
 {
     $path = ROOT_DIR . str_replace('//', '/', '/output/' . $uri . '/');
     if (is_file($path . $language . '.html')) {
+        header('Content-type: text/html; charset=utf-8');
         exit(file_get_contents($path . $language . '.html'));
     }
     if (is_file($path . 'en.html')) {
+        header('Content-type: text/html; charset=utf-8');
         exit(file_get_contents($path . '/en.html'));
     }
 }
-function findAdAndExit(string $file): void
+function findAdAndExit(string $file, string $mime): void
 {
     $path = ROOT_DIR . '/ads/' . date('Y-m');
     if (is_file($path . '/' . $file)) {
+        header('Content-type: ' . $mime);
         exit(file_get_contents($path . '/' . $file));
     }
     if (is_file(ROOT_DIR . '/ads/0000-00/' . $file)) {
+        header('Content-type: ' . $mime);
         exit(file_get_contents(ROOT_DIR . '/ads/0000-00/' . $file));
     }
 }
 $uri = trim($_SERVER['REQUEST_URI'] ?? '', '/');
 $language = 'en';
 if ($uri === 'ad.jpg') {
-    findAdAndExit('ad.jpg');
+    findAdAndExit('ad.jpg', 'image/jpeg');
 }
 if ($uri === 'ad.lnk') {
-    findAdAndExit('link.txt');
+    findAdAndExit('link.txt', 'txt/plain');
 }
 if ($uri === '') {
     findAndExit($uri, $language);
@@ -35,4 +39,5 @@ if ($uri === '') {
 if (!str_contains($uri, '.')) {
     findAndExit($uri, $language);
 }
+header('Content-type: text/html; charset=utf-8', true, 404);
 findAndExit('404', $language);

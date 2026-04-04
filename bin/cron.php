2a22efb277df3778c2dc43603a6f46aaf43cceef
Björn Büttner <Idrinth@users.noreply.github.com>
2026-04-04 18:19:03 +0200

    initial add support

--- bin/cron.php
+++ bin/cron.php
@@ -1,1 +1,17 @@
 <?php
+
+define("ROOT_DIR", dirname(__DIR__));
+
+$template = file_get_contents(ROOT_DIR . '/templates/template.html');
+
+$releases = [];
+foreach (scandir(ROOT_DIR . '/posts') as $folder) {
+    $meta = json_decode(file_get_contents(ROOT_DIR . '/posts/' . $folder . '/meta.json'));
+    $releases[$folder] = $meta;
+    if (is_dir(ROOT_DIR . '/output/' . $folder)) {
+        continue;
+    }
+    $english = file_get_contents(ROOT_DIR . '/posts/' . $folder . '/en.md');
+    $german = file_get_contents(ROOT_DIR . '/posts/' . $folder . '/de.md');
+    $french = file_get_contents(ROOT_DIR . '/posts/' . $folder . '/fr.md');
+}
